4b4a5941a516ae5bf386bb87ed17eb936c0d8600
kirkwadep1 <105302776+kirkwadep1@users.noreply.github.com>
2022-06-02 21:43:43 -0500

    Add files via upload

---
+++ project1-main/src/main/java/com/project1/services/CartServiceImpl.java
@@ -0,0 +1,8 @@
+package com.project1.services;
+
+public class CartServiceImpl {
+    /*
+        This was going to be implemented separately from the user class but
+        because of time constrains userservice will handle all of the carts as well
+     */
+}
